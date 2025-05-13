425a3d86371639586ffb30f08dd5ecbeb8b9c2d0
SarahTurna <sarahmahbub2@gmail.com>
2025-05-13 10:43:49 +0600

    :zap: anonKey hidden

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notes_app/controller/auth/login_controller.dart';
+import 'package:flutter_notes_app/controller/auth/registration_controller.dart';
+import 'package:flutter_notes_app/controller/home/notes_controller.dart';
+import 'package:flutter_notes_app/helpers/app_routes.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -10,6 +14,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+
+  Get.lazyPut(() => LoginController());
+  Get.lazyPut(() => RegisterController());
+  Get.lazyPut(() => NotesController());
+  await ScreenUtil.ensureScreenSize();
 
   runApp(const MyApp());
 }
@@ -24,12 +33,13 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       designSize: const Size(428, 926),
-      child: GetMaterialApp(
+      child: GetMaterialApp.router(
         debugShowCheckedModeBanner: false,
-        navigatorKey: Get.key,
         defaultTransition: Transition.noTransition,
-
         transitionDuration: const Duration(milliseconds: 300),
+        routerDelegate: appRouter.routerDelegate,
+        routeInformationParser: appRouter.routeInformationParser,
+        routeInformationProvider: appRouter.routeInformationProvider,
       ),
     );
   }
